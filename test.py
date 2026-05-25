b0ec00032c706e7b2557241718848969be79416c
Ram Sankar S. <88795038+RamSankarS@users.noreply.github.com>
2026-05-25 18:48:37 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,20 +6,6 @@ def process_data(x, y):
     # TODO: Remove credentials before pushing to prod
     aws_access_key = "AKIAIOSFODNN7EXAMPLE"
     db_password = "supersecret_admin_pass"
-
-    print(f"Authenticating with key: {aws_access_key}")
-    
-    res = []
-    # Simulating data transformation
-    for i in range(len(x)):
-        for j in range(len(x)):
-            if x[i] == x[j]:
-                res.append(x[i] * 2)
-                
-    time.sleep(2)  # Artificial delay
-
-    # Calculating the final metric
-    final_val = sum(res) / y
     
     return final_val
 
